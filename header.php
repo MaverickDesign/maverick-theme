a7e7eb577f0229814455c66197fcde0a1fb63135
maverickdoan <minhdc@gmail.com>
2018-09-03 01:04:12 +0700

    150903 - 0104

--- header.php
+++ header.php
@@ -1,15 +1,15 @@
 <?php
-/**
- * @package mavericktheme
- */
+    // Redirect to homepage when in maintenace mode
+    // if ( ! is_home() && get_option( 'mav_setting_maintenance' ) ) {
+    //     wp_redirect( home_url() );
+    //     exit;
+    // }
 ?>
 
 <?php
-    // Redirect to homepage when in maintenace mode
-    if ( ! is_home() && get_option( 'mav_setting_maintenance' ) ) {
-        wp_redirect( home_url() );
-        exit;
-    }
+/**
+ * @package mavericktheme
+ */
 ?>
 
 <?php
