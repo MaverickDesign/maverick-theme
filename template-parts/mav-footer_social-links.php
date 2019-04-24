afe9eff6d41b2c8745db5f4cbdf530611e5bbfaf
maverickdoan <minhdc@gmail.com>
2019-04-24 11:09:30 +0700

    190424 - 1109

--- template-parts/mav-footer_social-links.php
+++ template-parts/mav-footer_social-links.php
@@ -5,9 +5,9 @@
 
 if ( function_exists('mavf_social_links') ) {
     printf('<section id="mavid-site-footer-social" class="mav-footer-socials-wrapper mav-footer__socials--wrp">');
-        printf('<div class="mav-footer-socials-ctn mav-footer__socials--ctn">');
+        printf('<div class="mav-footer__socials--ctn">');
             printf(
-                '<p class="mav-text--xl mav-text--center"><strong>%1$s</strong> %2$s</p>',
+                '<p class="mav-text--center"><strong>%1$s</strong> %2$s</p>',
                 get_bloginfo( 'name' ), __( 'trên <span class="mav-no-break">mạng xã hội</span>', 'mavericktheme' )
             );
             mavf_social_links();
